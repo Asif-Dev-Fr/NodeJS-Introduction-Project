50f7d5eaa45be70d625154f2b01d5f5e83546450
Kassam Ali Asif <asif.k.dev@gmail.com>
2020-06-30 12:01:45 +0200

    commit sur 'master'

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ const database = new Datastore('database.db');
 database.loadDatabase();
 
 // Send information from the client to the server :
-app.use(express.json({limit: '1mb'}));
+app.use(express.json({ limit: '1mb' }));
 // Data sont envoyés depuis le client au server (post)
-app.post('/api', (request, response) =>{
+app.post('/api', (request, response) => {
     console.log('Server side :')
     console.log(request.body);
     // insertion en bdd : 
@@ -49,64 +49,84 @@ app.get('/getAllData', (request, response) => {
 });
 
 // Retrieve data from API on the server site : 
-    // Add API KEY with lat and lon to find the weather here : 
-    app.get('/weather/:latlon', async (request, response) => {
-        // console.log(request); 
+// Add API KEY with lat and lon to find the weather here : 
+app.get('/weather/:latlon', async (request, response) => {
+    // console.log(request); 
 
-        const latlon = request.params.latlon.split(',');
-        // console.log(latlon);
-        const lat = latlon[0];
-        const lon = latlon[1];
+    const latlon = request.params.latlon.split(',');
+    // console.log(latlon);
+    const lat = latlon[0];
+    const lon = latlon[1];
 
-        const api = {
-            key: process.env.API_KEY,
-            base: process.env.BASE
-        }
-        // Weather API
-        const fetch_response = await fetch(`${api.base}weather?lat=${lat}&lon=${lon}&appid=${api.key}`);
-        const json_data = await fetch_response.json();
+    const api = {
+        key: process.env.API_KEY,
+        base: process.env.BASE
+    }
+    // Weather API
+    const fetch_response = await fetch(`${api.base}weather?lat=${lat}&lon=${lon}&appid=${api.key}`);
+    const json_data = await fetch_response.json();
 
-        // Air Quality API : 
-        const aq_response = await fetch(`https://api.openaq.org/v1/latest?coordinates=${lat},${lon}`);
-        const aq_data = await aq_response.json();
+    // Air Quality API : 
+    const aq_response = await fetch(`https://api.openaq.org/v1/latest?coordinates=${lat},${lon}`);
+    const aq_data = await aq_response.json();
 
-        // Envoie des datas des 2 API :
-        const allData = {
-            weatherData : json_data,
-            airQualityData : aq_data
-        }
+    // Envoie des datas des 2 API :
+    const allData = {
+        weatherData: json_data,
+        airQualityData: aq_data
+    }
 
-        response.json(allData);
+    response.json(allData);
 
-    });
+});
+
+
+app.get('/city/:query', async (request, response) => {
+
+    const query = request.params.query;
+
+    const api = {
+        key: process.env.API_KEY,
+        base: process.env.BASE
+    }
+
+    // Weather :
+    const weather_api = `${api.base}weather?q=${query}&units=metric&APPID=${api.key}`;
+    const weather_response = await fetch(weather_api);
+    const weather_data = await weather_response.json();
+
+
+    // Air Quality :
+    const airQuality_api = `https://api.openaq.org/v1/latest?city=${query}`;
+    const airQuality_response = await fetch(airQuality_api);
+    const airQuality_data = await airQuality_response.json();
+
+    const data = {
+        weatherQuery: weather_data,
+        airQualityQuery: airQuality_data
+    }
+    response.json(data)
+});
+
+// Create new database : 
+const newDatabase = new Datastore('newDatabase.db');
+// création db
+newDatabase.loadDatabase();
+
+// Data sont envoyés depuis le client au server (post)
+app.post('/database', (request, response) =>{
+    console.log(request.body);
+    // insertion en bdd : 
+    newDatabase.insert(request.body);
+    // console.log(database);
+    response.json({
+        statut: 'success',
+        city: request.body.city,
+        latitude: request.body.lat,
+        longitude: request.body.lon,
+        weather: request.body.weather,
+        airQ: request.body.airQ,
         
-
-    app.get('/city/:query', async (request, response) => {
-
-        const query = request.params.query;
-
-        const api = {
-            key: process.env.API_KEY,
-            base: process.env.BASE
-        }
-
-        // Weather :
-        const weather_api = `${api.base}weather?q=${query}&units=metric&APPID=${api.key}`;
-        const weather_response = await fetch(weather_api);
-        const weather_data = await weather_response.json();
-
-
-        // Air Quality :
-        const airQuality_api = `https://api.openaq.org/v1/latest?city=${query}`;
-        const airQuality_response = await fetch(airQuality_api);
-        const airQuality_data = await airQuality_response.json();
-
-        const data = {
-            weatherQuery : weather_data,
-            airQualityQuery : airQuality_data
-        }
-        response.json(data)
     });
+});
 
-
-            
--- public/logs/weather.js
+++ public/logs/weather.js
@@ -1,4 +1,5 @@
-if ('geolocation' in navigator) {
+if ('geolocation' in navigator) 
+{
     console.log('Client side :  geolocation available');
     // position peut être changer par ce qu'on veut (value)
     navigator.geolocation.getCurrentPosition(async (position) => {
@@ -21,23 +22,26 @@ if ('geolocation' in navigator) {
 
         // Air Quality :
 
-        if(data_json.airQualityData.results.length != 0) {
+        if(data_json.airQualityData.results.length != 0) 
+        {
             const aqValue = document.getElementById('aqValue');
             aqValue.textContent = data_json.airQualityData.results[0].measurements[0].value;
 
             const aqUnit = document.getElementById('aqUnit');
             aqUnit.textContent = data_json.airQualityData.results[0].measurements[0].unit;
 
-        } else {
+        } else 
+        {
             document.getElementById('airQuality').style.display = "none"
         }
     } 
     )}
-else {
+else 
+{
     console.log('geolocation not available');
 }
 
-const search = async (event) => {
+const search = async () => {
     const query = document.getElementById('findCity').value;
     // console.log(query);
 
@@ -47,16 +51,65 @@ const search = async (event) => {
     console.log(data);
 
     document.getElementById('hiddenData').style.display = "block";
-    if (typeof data.weatherQuery.main != "undefined") {
+    if (typeof data.weatherQuery.main != "undefined") 
+    {
         document.getElementById('searchedCity').textContent = `${data.weatherQuery.name} : `;
         document.getElementById('searchedDegree').textContent = `${data.weatherQuery.main.temp}°`;
         document.getElementById('searchedDescription').textContent = `(${data.weatherQuery.weather[0].main})`;
+
+        if(data.airQualityQuery.results.length != 0) 
+        {
+            document.getElementById('aqValueQuery').textContent = data.airQualityQuery.results[0].measurements[0].value;
+
+            if (document.getElementById('aqUnitQuery').style.display = "none") {
+                document.getElementById('aqUnitQuery').style.display = "inline-block";
+            }
+
+            document.getElementById('aqUnitQuery').textContent = data.airQualityQuery.results[0].measurements[0].unit;            
+        }
+        else 
+        {
+            document.getElementById('aqValueQuery').textContent = 'Data not found';
+            document.getElementById('aqUnitQuery').style.display = "none";
+        }
     }
-    else { 
+    else 
+    { 
         document.getElementById('searchedCity').textContent = ' Pas de data ! Entrer le nom d\'une autre ville !';
         document.getElementById('searchedDegree').style.display = "none";
         document.getElementById('searchedDescription').style.display = "none";
     }
+
+
+    // Send information back to the server to be able to send them in the database : 
+    const lat = data.weatherQuery.coord.lat;
+    const lon = data.weatherQuery.coord.lon;
+    const weather = data.weatherQuery.main;
+    const city = data.weatherQuery.name;
+    const airQ = data.airQualityQuery.results[0].measurements[0];
+
+    const dataSendToDB = {
+        city,
+        lat,
+        lon,
+        weather,
+        airQ,
+    }
+    
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataSendToDB),
+    }
+
+    const db_response = await fetch('/database', options);
+    const db_json = await db_response.json();
+    console.log(db_json);
+
+
+
 }
 
 
